015b544f2bfcc9ca24e417ab2982474412321f2d
Jangts <microivan@live.com>
2018-07-18 21:23:49 +0800

    2123

---
+++ samples/app/SPA/images/main.js
@@ -0,0 +1,14 @@
+/*!
+ * tanguage script compiled code
+ *
+ * Datetime: Wed, 18 Jul 2018 08:54:06 GMT
+ */;
+// tang.config({});
+tang.init().block([
+    '$_/dom/',
+    '$_/app/SPA/'
+], function (pandora, root, imports, undefined) {
+    var SPA = imports['$_/app/spa/'];
+    var myapp = new SPA;
+}, true);
+//# sourceMappingURL=main.js.map
